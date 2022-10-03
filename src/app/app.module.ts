4f096bdb439804c855a0858fad3787bc9008fb3f
Jose E. Martinez <30488583+je-martinez@users.noreply.github.com>
2022-10-02 22:56:57 -0600

    feat: create slice to export reducer, actions, counter for feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { CounterReducer } from 'src/store/slices/counter.slice';
 
 @NgModule({
   declarations: [AppComponent, CounterComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({
+      counter: CounterReducer,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  CounterActions,
+  CounterSelectors,
+} from 'src/store/slices/counter.slice';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent implements OnInit {
+  constructor(private readonly store: Store) {}
+  readonly count$ = this.store.select(CounterSelectors.selectValue);
+  readonly incrementCount$ = this.store.select(
+    CounterSelectors.selectIncrementCount
+  );
+  readonly decrementCount$ = this.store.select(
+    CounterSelectors.selectDecrementCount
+  );
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onDecrement() {
+    this.store.dispatch(CounterActions.decrement());
   }
 
+  onIncrement() {
+    this.store.dispatch(CounterActions.increment());
+  }
 }

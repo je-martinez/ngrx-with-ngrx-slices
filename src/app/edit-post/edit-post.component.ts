f1fd81216b102959423620cb810c902328205aca
Jose E. Martinez <30488583+je-martinez@users.noreply.github.com>
2022-10-07 19:38:50 -0600

    feat: work in progress edit post

--- src/app/edit-post/edit-post.component.ts
+++ src/app/edit-post/edit-post.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Subject, takeUntil } from 'rxjs';
+import { PostsActions, PostsSelectors } from 'src/store/slices/posts.slice';
+import { Actions, ofType } from '@ngrx/effects';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-post',
   templateUrl: './edit-post.component.html',
-  styleUrls: ['./edit-post.component.scss']
+  styleUrls: ['./edit-post.component.scss'],
 })
-export class EditPostComponent implements OnInit {
+export class EditPostComponent implements OnInit, OnDestroy {
+  postForm: FormGroup = new FormGroup({
+    userId: new FormControl<number>(0, [Validators.required]),
+    id: new FormControl<number>(0, [Validators.required]),
+    title: new FormControl<string>('', [Validators.required]),
+    body: new FormControl<string>('', [Validators.required]),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private readonly store: Store,
+    private readonly actions$: Actions,
+    private router: Router
+  ) {
+    this.actions$
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        ofType(PostsActions.deletePost.success)
+      )
+      .subscribe(() => {
+        this.router.navigate(['/posts']);
+      });
   }
 
+  readonly post$ = this.store.select(PostsSelectors.selectSelectedPost);
+  readonly loading$ = this.store.select(
+    PostsSelectors.selectLoadingCreateOrUpdatePost
+  );
+  private readonly unsubscribe$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.post$.pipe(takeUntil(this.unsubscribe$)).subscribe((post) => {
+      if (post) {
+        this.postForm.patchValue(post);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  onSubmit() {
+    if (this.postForm.invalid) {
+      return;
+    }
+    this.store.dispatch(PostsActions.updatePost.trigger(this.postForm.value));
+  }
 }
--- src/store/slices/posts.slice.ts
+++ src/store/slices/posts.slice.ts
@@ -1,22 +1,28 @@
 import { PostResponse } from '../../models/api/post.responses.models';
-import { createSlice, noopReducer, PayloadAction } from 'ngrx-slice';
+import { createSlice, PayloadAction } from 'ngrx-slice';
 import { Post } from 'src/models/entities/posts.models';
 
 export interface PostsState {
   posts: PostResponse[];
+  postToCreateOrUpdate: Post | undefined | null;
   selectedPost: PostResponse | undefined | null;
   loadingGetPosts: boolean;
   loadingDeletePost: boolean;
+  loadingCreateOrUpdatePost: boolean;
   errorGetPosts: string | any;
+  errorCreateOrUpdatePost: string | any;
   errorDeletePost: string | any;
 }
 
 export const initialState: PostsState = {
   posts: [],
   selectedPost: null,
+  postToCreateOrUpdate: null,
   loadingGetPosts: false,
+  loadingCreateOrUpdatePost: false,
   loadingDeletePost: false,
   errorGetPosts: null,
+  errorCreateOrUpdatePost: null,
   errorDeletePost: null,
 };
 
@@ -28,29 +34,6 @@ export const {
   name: 'posts',
   initialState,
   reducers: {
-    selectedPost: (
-      state: PostsState,
-      action: PayloadAction<{ value: Post | undefined | null }>
-    ) => {
-      state.selectedPost = action.value ? { ...action.value } : null;
-    },
-    deletePost: {
-      trigger: (state: PostsState) => {
-        state.loadingDeletePost = true;
-      },
-      success: (state: PostsState, action: PayloadAction<{ id: number }>) => {
-        state.posts = state.posts.filter((p) => p.id != action.id);
-        state.selectedPost = null;
-        state.loadingDeletePost = false;
-      },
-      failure: (
-        state: PostsState,
-        action: PayloadAction<{ error: string | any }>
-      ) => {
-        state.loadingDeletePost = false;
-        state.errorDeletePost = action.error;
-      },
-    },
     getPosts: {
       trigger: (state: PostsState) => {
         state.loadingGetPosts = true;
@@ -68,6 +51,52 @@ export const {
       ) => {
         state.loadingGetPosts = false;
         state.errorGetPosts = action.error;
+      },
+    },
+    selectedPost: (
+      state: PostsState,
+      action: PayloadAction<{ value: Post | undefined | null }>
+    ) => {
+      state.selectedPost = action.value ? { ...action.value } : null;
+    },
+    updatePost: {
+      trigger: (state: PostsState, action: PayloadAction<{ post: Post }>) => {
+        state.postToCreateOrUpdate = action.post;
+        state.loadingCreateOrUpdatePost = true;
+      },
+      success: (state: PostsState, action: PayloadAction<{ post: Post }>) => {
+        const indexToEdit = state.posts.findIndex(
+          (item) => item.id == action.post.id
+        );
+        if (indexToEdit >= 0) {
+          state.posts[indexToEdit] = { ...action.post };
+        }
+        state.selectedPost = null;
+        state.loadingCreateOrUpdatePost = false;
+      },
+      failure: (
+        state: PostsState,
+        action: PayloadAction<{ error: string | any }>
+      ) => {
+        state.errorCreateOrUpdatePost = action.error;
+        state.loadingCreateOrUpdatePost = false;
+      },
+    },
+    deletePost: {
+      trigger: (state: PostsState) => {
+        state.loadingDeletePost = true;
+      },
+      success: (state: PostsState, action: PayloadAction<{ id: number }>) => {
+        state.posts = state.posts.filter((p) => p.id != action.id);
+        state.selectedPost = null;
+        state.loadingDeletePost = false;
+      },
+      failure: (
+        state: PostsState,
+        action: PayloadAction<{ error: string | any }>
+      ) => {
+        state.loadingDeletePost = false;
+        state.errorDeletePost = action.error;
       },
     },
   },
